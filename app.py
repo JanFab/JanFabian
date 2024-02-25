7d39999e5a3cd8cd56b2e6b2119a1c194e0b00e9
jan <jan.fabian@student.tuke.sk>
2024-02-25 21:52:11 +0100

    Fix

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-from send_email import send_email
-from sqlalchemy.sql import func
+# from send_email import send_email
+# from sqlalchemy.sql import func
 
 import yfinance as yf
 
@@ -10,18 +10,18 @@ app=Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/height_collector'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wvbuzieydcngrr:6ff8a9a1c7a2187ec1f0f897e064956b6509627318514f308817b1a3433bb074@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d2uj132js0farb?sslmode=require'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kbqlahjvzmlizu:dd8dafab365744375f74fb7f47e6d770afe7ce684ae1e856d307463bb46c8e8a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d7911aqu6f6oj5?sslmode=require'
-db=SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kbqlahjvzmlizu:dd8dafab365744375f74fb7f47e6d770afe7ce684ae1e856d307463bb46c8e8a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d7911aqu6f6oj5?sslmode=require'
+# db=SQLAlchemy(app)
 
-class Data(db.Model):
-    __tablename__="data"
-    id=db.Column(db.Integer, primary_key=True)
-    email_=db.Column(db.String(120), unique=True)
-    height_=db.Column(db.Integer)
-    
-    def __init__(self, email_, height_):
-        self.email_ = email_
-        self.height_ = height_
+# class Data(db.Model):
+#     __tablename__="data"
+#     id=db.Column(db.Integer, primary_key=True)
+#     email_=db.Column(db.String(120), unique=True)
+#     height_=db.Column(db.Integer)
+#
+#     def __init__(self, email_, height_):
+#         self.email_ = email_
+#         self.height_ = height_
 
 
 @app.route('/gallery/')
@@ -80,32 +80,34 @@ def gallery():
 def home():
     return render_template("home.html")
 
+
 @app.route('/hobbies/')
 def hobbies():
     return render_template("hobbies.html")
+
 
 @app.route('/experience/')
 def experience():
     return render_template("experience.html")
 
 
-@app.route("/success/", methods=['POST'])
-def success():
-    if request.method == 'POST':
-        email=request.form["email_name"]
-        height=request.form["height_name"]
-        
-        if db.session.query(Data).filter(Data.email_==email).count() == 0:
-            data=Data(email, height)
-            db.session.add(data)
-            db.session.commit()
-            average_height = db.session.query(func.avg(Data.height_)).scalar()
-            average_height = round(average_height)
-            count = db.session.query(Data.height_).count()
-            send_email(email, height, average_height, count)
-            return render_template("success.html")
-        else:
-            return render_template('experiences.html', text="Seems like we've got something from that email address already!")
+# @app.route("/success/", methods=['POST'])
+# def success():
+#     if request.method == 'POST':
+#         email=request.form["email_name"]
+#         height=request.form["height_name"]
+#
+#         if db.session.query(Data).filter(Data.email_==email).count() == 0:
+#             data=Data(email, height)
+#             db.session.add(data)
+#             db.session.commit()
+#             average_height = db.session.query(func.avg(Data.height_)).scalar()
+#             average_height = round(average_height)
+#             count = db.session.query(Data.height_).count()
+#             send_email(email, height, average_height, count)
+#             return render_template("success.html")
+#         else:
+#             return render_template('experiences.html', text="Seems like we've got something from that email address already!")
 
 if __name__ == "__main__":
     # app.debug=True
--- send_email.py
+++ send_email.py
@@ -1,21 +1,21 @@
-from email.mime.text import MIMEText
-import smtplib
-
-def send_email(email, height, average_height, count):
-    from_email="srnka.les@gmail.com"
-    from_password="fabianovci"
-    to_email=email
-
-    subject="Údaje o výške'"
-    message="Dobrý deň, zasielame Vám údaje o vašej výške <strong>%s</strong>. Priemerná výška respondentov je <strong>%s</strong> čo je vypočítané z celkového počtu <strong>%s</strong>. Prajem pekný zvyšok dňa." % (height, average_height, count)
-
-    msg=MIMEText(message, 'html')
-    msg['Subject']=subject
-    msg['To'] = to_email
-    msg['From'] = from_email
-
-    gmail=smtplib.SMTP('smtp.gmail.com', 587)
-    gmail.ehlo()
-    gmail.starttls()
-    gmail.login(from_email, from_password)
-    gmail.send_message(msg)
+# from email.mime.text import MIMEText
+# import smtplib
+#
+# def send_email(email, height, average_height, count):
+#     from_email="srnka.les@gmail.com"
+#     from_password="fabianovci"
+#     to_email=email
+#
+#     subject="Údaje o výške'"
+#     message="Dobrý deň, zasielame Vám údaje o vašej výške <strong>%s</strong>. Priemerná výška respondentov je <strong>%s</strong> čo je vypočítané z celkového počtu <strong>%s</strong>. Prajem pekný zvyšok dňa." % (height, average_height, count)
+#
+#     msg=MIMEText(message, 'html')
+#     msg['Subject']=subject
+#     msg['To'] = to_email
+#     msg['From'] = from_email
+#
+#     gmail=smtplib.SMTP('smtp.gmail.com', 587)
+#     gmail.ehlo()
+#     gmail.starttls()
+#     gmail.login(from_email, from_password)
+#     gmail.send_message(msg)
